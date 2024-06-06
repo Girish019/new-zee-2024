bf9f4c16545cca6c7b7f61900c829cfb514bf5bb
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-06-06 21:54:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
-from datetime import datetime
-
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from datetime import date, datetime 
+import pytz
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT, LOG_ID, RESTART_TXT
 
 class Bot(Client):
     def __init__(self):
@@ -46,8 +46,12 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
-            await test.delete()
+            tz = pytz.timezone('Asia/Kolkata')
+            today = date.today()
+            now = datetime.now(tz)
+            time = now.strftime("%H:%M:%S %p")
+            test = await self.send_message(chat_id = LOG_ID, text = RESTART_TXT.format(today, time))
+           # await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
