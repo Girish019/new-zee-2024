dab814be01de16ac6505dde5a47adb455f63cfc0
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-08-13 15:36:40 +0530

    Update scheduled_msg.py

--- plugins/scheduled_msg.py
+++ plugins/scheduled_msg.py
@@ -1,20 +1,20 @@
-import pandas as pd
-import requests
-import time
+#import pandas as pd
+#import requests
+#import time
 
 
-while True:
+#while True:
     
-    df=[]
-    def send_message():
-        bot_id = "6022200330:AAFGVSEpxX0M_jUahlxxVZlQ9xCZvG9D0_4"
-        chat_id = int(-1001328262780)
-        message = "hii buddy, msg from render for continuous ofter 1 hour"
-        url = f"https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id}&text={message}"
+    #df=[]
+    #def send_message():
+        #bot_id = "6022200330:AAFGVSEpxX0M_jUahlxxVZlQ9xCZvG9D0_4"
+        #chat_id = int(-1001328262780)
+        #message = "hii buddy, msg from render for continuous ofter 1 hour"
+        #url = f"https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id}&text={message}"
 
-        return requests.get(url).json()
+        #return requests.get(url).json()
 
 
-    send_message()
-    time.sleep(3600)
-    print("ok")
+    #send_message()
+    #time.sleep(3600)
+    #print("ok")
